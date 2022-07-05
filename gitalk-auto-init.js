6d33789e0cc9c72e5394f28552fc2767d6182be4
beimengyeyu <beimengyeyu@outlook.com>
2022-07-05 17:39:49 +0800

    谷歌收录

--- gitalk-auto-init.js
+++ gitalk-auto-init.js
@@ -11,7 +11,7 @@ const config = {
   username: 'beimengyeyu', // GitHub repository 所有者，可以是个人或者组织。对应Gitalk配置中的owner
   repo: 'beimengyeyu.github.io', // 储存评论issue的github仓库名，仅需要仓库名字即可。对应 Gitalk配置中的repo
   token: process.env.GITHUB_TOKEN, // 前面申请的 personal access token
-  sitemap: path.join(__dirname, './public/sitemap.xml'), // 自己站点的 sitemap 文件地址
+  sitemap: path.join(__dirname, './public/sitemap.txt'), // 自己站点的 sitemap 文件地址
   cache: true, // 是否启用缓存，启用缓存会将已经初始化的数据写入配置的 gitalkCacheFile 文件，下一次直接通过缓存文件判断
   gitalkCacheFile: path.join(__dirname, './gitalk-init-cache.json'), // 用于保存 gitalk 已经初始化的 id 列表
   gitalkErrorFile: path.join(__dirname, './gitalk-init-error.json'), // 用于保存 gitalk 初始化报错的数据
